2b9bca7f41bca17ea53589325a37d93bb2e14142
Kolipaka Akhil Surya <130474386+AkhilSuryaK@users.noreply.github.com>
2025-06-13 11:12:12 +0530

    Add files via upload

---
+++ network_tracker.py
@@ -0,0 +1,61 @@
+import dpkt
+import socket
+import pygeoip
+
+gi = pygeoip.GeoIP('GeoLiteCity.dat')
+
+def retKML(dstip, srcip):
+    dst = gi.record_by_name(dstip)
+    src = gi.record_by_name('106.222.228.220')  # Replace with your real IP
+    try:
+        dstlongitude = dst['longitude']
+        dstlatitude = dst['latitude']
+        srclongitude = src['longitude']
+        srclatitude = src['latitude']
+        kml = (
+            '<Placemark>\n'
+            '<name>%s</name>\n'
+            '<extrude>1</extrude>\n'
+            '<tessellate>1</tessellate>\n'
+            '<styleUrl>#transBluePoly</styleUrl>\n'
+            '<LineString>\n'
+            '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
+            '</LineString>\n'
+            '</Placemark>\n'
+        ) % (dstip, dstlongitude, dstlatitude, srclongitude, srclatitude)
+        return kml
+    except:
+        return ''
+
+def plotIPs(pcap):
+    kmlPts = ''
+    for (ts, buf) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            src = socket.inet_ntoa(ip.src)
+            dst = socket.inet_ntoa(ip.dst)
+            KML = retKML(dst, src)
+            kmlPts = kmlPts + KML
+        except:
+            continue
+    return kmlPts
+
+def main():
+    f = open('wire.pcap', 'rb')
+    pcap = dpkt.pcap.Reader(f)
+    kmlheader = '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n' \
+                '<Style id="transBluePoly">' \
+                '<LineStyle>' \
+                '<width>1.5</width>' \
+                '<color>501400E6</color>' \
+                '</LineStyle>' \
+                '</Style>'
+    kmlfooter = '</Document>\n</kml>\n'
+    kmldoc = kmlheader + plotIPs(pcap) + kmlfooter
+    with open('network_traffic.kml', 'w') as kmlfile:
+        kmlfile.write(kmldoc)
+    print("[+] KML file created: network_traffic.kml")
+
+if __name__ == '__main__':
+    main()
